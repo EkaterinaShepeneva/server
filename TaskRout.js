c2236242656ae16271b30340c3596aab5710dff1
Ekaterina Shepeneva <ekaterinashepeneva@gmail.com>
2022-07-21 17:05:25 +0300

    create router and send answer

--- TaskRout.js
+++ TaskRout.js
@@ -1,12 +1,16 @@
-// const express= require("express")
-// const router=express.Router();
+const express= require("express")
+const router=express.Router();
 
-// router.get('/', (req, res) => {
-//     res.send('Hello World! and kate')
-//   })
-  
-//   router.post('/', (req, res) => {
-//       console.log(req.body)
-//       res.send('Hello World! and kate')
-//     })
-// module.exports = router;
+router.get('/tasks/5', function (req, res, next) {
+    console.log('its router, params = ', req.query);
+    const answer = `Кажется кто-то хочет вывести ${req.query.page} страницу
+    массива, отсортированного ${(req.query.order=='asc')?('по нормальному'):('задом наперёд')} 
+    и отфильтрованного по фильтру ${req.query.filterBy?((req.query.filterBy=='done')?('СДЕЛАННЫЕ'):("НЕ_СДЕЛАННЫЕ")):('ВСЕ')}?
+    
+    
+    А больше ты ничего не хочешь ?!`
+    res.send(answer)
+    next()
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const router = require('./TaskRout')
 const app = express()
 const port = 3000
 app.use(express.json())
@@ -7,7 +8,10 @@ app.listen(port, () => {
   console.log('hello, kate');
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.use(router)
+
+app.get('/', function (req, res, next) {
+    console.log('hello');
+    next(); 
+})
 
