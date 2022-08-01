6a623633b910a3148ff8655294f451c5c2fb8230
Ekaterina Shepeneva <ekaterinashepeneva@gmail.com>
2022-08-01 13:24:15 +0300

    get individual tasks

--- routes/task/tasks.get.js
+++ routes/task/tasks.get.js
@@ -9,12 +9,16 @@ router.get('/tasks', auth, async (req, res, next) => {
 
   try {
 
-    const { page = 1, filterBy, pp = 5, order } = req.query;
+    const { page = 1, filterBy, pp = 5, order, login } = req.query;
     let tasks;
     const whereCondition = filterBy ? { done: FILTER_BY[filterBy] } : {}
 
+    const { dataValues } = await db.User.findOne({
+      where: { login },
+    })
+
     tasks = await db.Task.findAndCountAll({
-      where: whereCondition,
+      where: [whereCondition, { user_id: dataValues.userId }],
       order: [['createdAt', order === 'asc' ? SORT_BY.ASC : SORT_BY.DESC,]],
       limit: pp,
       offset: (+page - 1) * +pp
